a9eb73d496f3eaafb6f27c79817471b51a180c8d
gi613ts <malkowski.m.a@gmail.com>
2025-08-15 15:12:00 +0200

    1.4 Input

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 
 
 def main():
+
+    if len(sys.argv) != 2:
+        print("this script takes one argument (the prompt string)")
+        sys.exit(1)
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    prompt = ""
+    prompt = sys.argv[1]
 
     response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents=prompt
     )
 
     print(response.text)
