94c3e7bb229021bf607165c4ac6ff025f44e545b
gi613ts <malkowski.m.a@gmail.com>
2025-08-15 15:39:10 +0200

    1.6 Verbose

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 
 def main():
 
-    if len(sys.argv) != 2:
-        print("this script takes one argument (the prompt string)")
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("this script takes one argument (the prompt string) and an optional --verbose switch")
         sys.exit(1)
 
     load_dotenv()
@@ -17,16 +18,28 @@ def main():
 
     prompt = sys.argv[1]
 
+    messages = [types.Content(
+    role="user", 
+    parts=[types.Part(text=prompt)]),
+    ]
+
     response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=prompt
+    contents=messages
     )
-
-    print(response.text)
-
-    usage_numbers = response.usage_metadata
-    print(f"Prompt tokens: {usage_numbers.prompt_token_count}")
-    print(f"Response tokens: {usage_numbers.candidates_token_count}")
+    
+    def verbose_output():
+        usage_numbers = response.usage_metadata
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {usage_numbers.prompt_token_count}")
+        print(f"Response tokens: {usage_numbers.candidates_token_count}")
+    
+    def default_output():
+        print(response.text)
+    
+    default_output()
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_output()
 
 if __name__ == "__main__":
     main()
