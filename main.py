b69b17cc8308c27778bf72312d313b0ca308e9c2
gi613ts <malkowski.m.a@gmail.com>
2025-09-02 22:43:03 +0200

    Refactoring for 4.1

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def main():
     if len(sys.argv) < 2 or len(sys.argv) > 3:
         print("this script takes one argument (the prompt string) and an optional --verbose switch")
         sys.exit(1)
+    
+    verbose = "--verbose" in sys.argv
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -21,46 +23,68 @@ def main():
 
     prompt = sys.argv[1]
 
-    messages = [types.Content(
-    role="user", 
-    parts=[types.Part(text=prompt)]),
-    ]
+    if verbose:
+        print(f"User prompt: {prompt}")
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
+    generate_content(client, messages, verbose)
+
+
+
+
+def generate_content(client, messages, verbose):
 
     response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
-    config=types.GenerateContentConfig(
-    tools=[available_functions], system_instruction=system_prompt)
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+        )
     )
     
-    def verbose_output():
-        print(response.text)
-        if response.function_calls:
-            for function_call in response.function_calls:
-                function_result = call_function(function_call, verbose=True)
-                if not function_result.parts[0].function_response.response:
-                    raise Exception("Function call failed, function_result object is None")
-                else:
-                    print(f"-> {function_result.parts[0].function_response.response}")
-
+    if verbose:
         usage_numbers = response.usage_metadata
-        print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {usage_numbers.prompt_token_count}")
         print(f"Response tokens: {usage_numbers.candidates_token_count}")
+
+    if not response.function_calls:
+        return response.text
+
+    function_results = []
+    for function_call in response.function_calls:
+        function_result = call_function(function_call, verbose)
+        if (
+            not function_result.parts or
+            not function_result.parts[0].function_response
+        ):
+            raise Exception("Function call failed, function_result object is None")
+        if verbose:
+            print(f"-> {function_result.parts[0].function_response.response}")
+        function_results.append(function_result.parts[0])
+        print(function_results)
     
-    def default_output():
-        print(response.text)
-        if response.function_calls:
-            for function_call in response.function_calls:
-                function_result = call_function(function_call, verbose=False)
-                if not function_result.parts[0].function_response.response:
-                    raise Exception("Function call failed, function_result object is None")
-    
-    
-    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
-        verbose_output()
-    else:
-        default_output()
+    if function_results == []:
+        raise Exception("Function result list is empty")
+
+
+    """
+    for response in response.candidates:
+        messages.append(response)
+
+    #print("response candidates: ", response.candidates)
+    #print("response candidate item: ", response.candidates[0])
+    #print("response candidate item content: ", response.candidates[0].content)
+
+    print("call output: ", call_output)
+    print("messages: ", messages)
+
+    """
+
+
+
+
+
 
 
 def call_function(function_call_part, verbose=False):
